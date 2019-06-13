39e246ef50f522c6db7cce0f5afcdc2b4eee2e83
andrea <andreasilas.as@gmail.com>
2019-06-13 19:14:58 +0200

    Subo el proyecto al repositorio de GitHub

---
+++ wp-content/themes/casper-child/functions.php
@@ -0,0 +1,48 @@
+<?php
+function my_theme_enqueue_styles() {
+
+ $parent_style = 'parent-style'; // Estos son los estilos del tema padre recogidos por el tema hijo.
+
+ wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+ wp_enqueue_style( 'child-style',
+ get_stylesheet_directory_uri() . '/style.css',
+ array( $parent_style ),
+ wp_get_theme()->get('Version')
+ );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+/* Código para crear un widget */
+if (function_exists('register_sidebar')) {
+        register_sidebar(array(
+            'name' => 'Sidebar Widgets',
+            'id'   => 'sidebar-widgets',
+            'description'   => 'These are widgets for the sidebar.',
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h2>',
+            'after_title'   => '</h2>'
+        ));
+    }
+
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+
+if ( !function_exists( 'chld_thm_cfg_add_parent_dep' ) ):
+function chld_thm_cfg_add_parent_dep() {
+    global $wp_styles;
+    array_unshift( $wp_styles->registered[ 'casper-style' ]->deps, 'parent-style' );
+}
+endif;
+add_action( 'wp_head', 'chld_thm_cfg_add_parent_dep', 2 );
+
+// END ENQUEUE PARENT ACTION
---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1561045554;s:7:"authKey";s:64:"-5}O=jdg U~{s-rHQ_;U*<jF=C~8Qx*SJ(g2hHh4l+qLv[cf-K^UB{W:Z51n)BCO";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3157;}
